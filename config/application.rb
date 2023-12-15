2e7624e4d5830ec7b71eedc61d748c146ffc6ffd
Rebelzob <karimdavid07@gmail.com>
2023-12-15 00:12:54 +0000

    Update Gemfile and application files

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module HelloRailsBackend
+  # Main application class for the HelloRailsBackend project.
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.1
