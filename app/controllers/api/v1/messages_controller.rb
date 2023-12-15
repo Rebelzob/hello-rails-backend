30b7fdd978d0ea14eb4c0b0294b5fe49a3d93786
Rebelzob <karimdavid07@gmail.com>
2023-12-15 00:04:14 +0000

    Add frozen_string_literal: true to all Ruby files

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,10 +1,16 @@
-class Api::V1::MessagesController < ApplicationController
-  def index
-    @message = Message.order('RANDOM()').first
-    if @message
-      render json: { message: @message.text, status: 'Success' }
-    else
-      render json: { message: 'No messages found', status: 'Error' }
+# frozen_string_literal: true
+
+module Api
+  module V1
+    class MessagesController < ApplicationController
+      def index
+        @message = Message.order('RANDOM()').first
+        if @message
+          render json: { message: @message.text, status: 'Success' }
+        else
+          render json: { message: 'No messages found', status: 'Error' }
+        end
+      end
     end
   end
 end
